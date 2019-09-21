12dabe21795b17c0fab114d1316f9be9e0e28e84
Zahin430 <zhprangon@mun.ca>
2019-09-21 19:43:40 -0230

    first commit

---
+++ Assets/Scripts/Enemy/FrenemyAttack.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrenemyAttack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+
+
+    Animator anim;
+    GameObject player;
+    GameObject enemy;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    bool playerInRange;
+    float timer;
+
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
+        anim = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(gameObject.name + "  says to " + other.gameObject.name + ": Stop touching me!");
+    }
+
+        void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
+        {
+            Attack();
+        }
+
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("PlayerDead");
+        }
+    }
+
+
+    void Attack()
+    {
+        timer = 0f;
+
+        if (playerHealth.currentHealth > 0)
+        {
+            playerHealth.TakeDamage(attackDamage);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/FrenemyMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrenemyMovement : MonoBehaviour
+{
+    
+    public void Awake()
+    {
+        Debug.Log("You have awakened a " + gameObject.name);
+    }
+
+}
